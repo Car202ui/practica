79682b753db5d36b9e6e672a2255678ad8228b15
Carlos Andres Iglesias Arrieta <ciglesias@EBSLTDA.LAN>
2021-04-17 20:42:43 -0500

    practica

---
+++ src/main/java/com/practica/proyectopractica/entity/Disquera.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.practica.proyectopractica.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author ciglesias
+ *
+ */
+
+@Entity
+@Table(name="disquera")
+public class Disquera {
+	
+
+}
